58ece495df33ea24e151fa73bb8dd5d240ab38e7
ianverger <ian.verger@gmail.com>
2022-08-01 17:25:46 -0400

    iancommit

--- enumerables.rb
+++ enumerables.rb
@@ -12,20 +12,91 @@ class Array
 
     def my_select(&prc) #{|num| num >1}
         arr = [] #[1,2,3]
-        self.my_each do |ele|
-            if prc.call(ele)
-                arr << ele 
-            end
-        end
-
+        self.my_each { |ele| arr << ele if prc.call(ele) }
         arr
     end
 
+    def my_reject(&prc)
+        arr = []
+        self.my_each { |ele| arr << ele if !prc.call(ele) }
+        arr
+    end
 
+    def my_any?(&prc)
+        self.my_each { |ele| return true if prc.call(ele) }
+
+        false
+    end
+
+    def my_all?(&prc)
+        self.my_each { |ele| return false if !prc.call(ele) }
+
+        true
+    end
+
+    def my_zip(*args)
+        result = Array.new(self.length) { Array.new(args.length + 1)}
+        all_args = args.unshift(self)
+       
+        (0...self.length).each do |row| 
+            (0...all_args.length).each do |col| 
+                result[row][col] = all_args[col][row]
+            end
+        end
+
+        result
+    end
+   
+    def my_rotate(n = 1)
+        arr = self.dup
+
+        if n > 0
+            n.times do |ele|
+                holder = arr.shift
+                arr.push(holder)
+            end
+        else
+            (n * -1).times do |ele|
+                holder = arr.pop
+                arr.unshift(holder)
+            end
+        end
+        return arr
+    end
+
+    def my_join(sep = "")
+        result = ""
+
+        self.each { |char| result += char + sep }
+        result
+    end
+
+    def my_reverse
+        result = []
+
+        i = self.length - 1
+        while i >= 0
+            result << self[i]
+            i -= 1
+        end
+        result
+    end
+
+    def my_flatten
+        return [self] if !self.is_a?(Array) 
+        arr = []
+        self.each { |ele| arr += my_flatten(ele) }
+        arr
+    end
+
+     # def flatten(data)
+    #     return [data] if !data.is_a?(Array)
+    #     flattened = []
+    #     data.each { |ele| flattened += flatten(ele) }
+    #     flattened
+    #   end
 end
 
-a = [1, 2, 3]
-p a.my_select { |num| num > 1 } # => [2, 3]
-p a.my_select { |num| num == 4 } # => []
+print [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
 
 
